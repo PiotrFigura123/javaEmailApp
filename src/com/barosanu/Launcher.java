d6e178a5dbccb8ccd8ea49ac366f7ed312bf18e3
Piotr Figura <pf>
2022-04-14 14:47:32 +0200

    Main Menu layout

--- src/com/barosanu/Launcher.java
+++ src/com/barosanu/Launcher.java
@@ -16,8 +16,8 @@ public class Launcher extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
-        Scene scene = new Scene(parent,300,300);
+        Parent parent = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
+        Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/com/barosanu/controller/MainWindowController.java
@@ -0,0 +1,22 @@
+package com.barosanu.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.web.WebView;
+
+public class MainWindowController {
+
+    @FXML
+    private TableView<?> emailTableView;
+
+    @FXML
+    private WebView emailWebView;
+
+    @FXML
+    private TreeView<?> emailsTreeView;
+
+    @FXML
+    void optionsAction() {
+    }
+}
