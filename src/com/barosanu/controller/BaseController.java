e5637babde3430a4520d0bce759acb35aa2af926
Piotr Figura <pf>
2022-04-14 16:36:44 +0200

    q q
    created controller

--- src/com/barosanu/controller/BaseController.java
+++ src/com/barosanu/controller/BaseController.java
@@ -15,4 +15,7 @@ public abstract class BaseController {
     }
 
 
+    public String getFxmlName() {
+        return fxmlName;
+    }
 }
--- src/com/barosanu/view/ViewFactory.java
+++ src/com/barosanu/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package com.barosanu.view;
 
 import com.barosanu.EmailMenager;
+import com.barosanu.controller.BaseController;
+import com.barosanu.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
     private EmailMenager emailMenager;
@@ -11,5 +19,23 @@ public class ViewFactory {
 
     public void showLoginWindow(){
         System.out.println("show colled");
+
+        BaseController controller = new LoginWindowController(emailMenager,this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(controller.getFxmlName()));
+        fxmlLoader.setController(controller);
+        Parent parent;
+        try{
+            parent = fxmlLoader.load();
+        }catch(IOException e){
+            e.printStackTrace();
+            return;
+        }
+        Scene scene = new Scene(parent);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+
+
     }
 }
