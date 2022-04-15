30e0dcbed6f5aeeb20580d2de3cf7f41b1e3a275
Piotr Figura <pf>
2022-04-15 12:43:52 +0200

    Enum with diff colors

--- src/com/barosanu/controller/OptionsWindowController.java
+++ src/com/barosanu/controller/OptionsWindowController.java
@@ -2,12 +2,18 @@ package com.barosanu.controller;
 
 
 import com.barosanu.EmailMenager;
+import com.barosanu.view.ColorTheme;
 import com.barosanu.view.ViewFactory;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
 
-public class OptionsWindowController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OptionsWindowController extends BaseController implements Initializable {
 
     public OptionsWindowController(EmailMenager emailMenager, ViewFactory viewFactory, String fxmlName) {
         super(emailMenager, viewFactory, fxmlName);
@@ -16,7 +22,7 @@ public class OptionsWindowController extends BaseController {
     private Slider fontSizePicker;
 
     @FXML
-    private ChoiceBox<?> themePicker;
+    private ChoiceBox<ColorTheme> themePicker;
 
     @FXML
     void applyBtnAction() {
@@ -26,5 +32,21 @@ public class OptionsWindowController extends BaseController {
     @FXML
     void cancelButtonAction() {
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setUpPicker();
+        setUpSizePicker();
+        
+
+    }
+
+    private void setUpSizePicker() {
+    }
+
+    private void setUpPicker() {
+        themePicker.setItems(FXCollections.observableArrayList(ColorTheme.values()));
+        themePicker.setValue(viewFactory.getColorTheme());
     }
 }
