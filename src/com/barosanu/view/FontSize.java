4fcb91121128992d7a46cbc29d2eeb6bd2ca42a0
Piotr Figura <pf>
2022-04-15 14:57:30 +0200

    denamically change styles of app

--- src/com/barosanu/view/FontSize.java
+++ src/com/barosanu/view/FontSize.java
@@ -3,5 +3,17 @@ package com.barosanu.view;
 public enum FontSize {
     SMALL,
     MEDIUM,
-    BIG
+    BIG;
+    public static String getCssPath(FontSize fontSize){
+        switch (fontSize){
+            case SMALL:
+                return "css/fontSmall.css";
+            case MEDIUM:
+                return "css/fontMedium.css";
+            case BIG:
+                return "css/fontBig.css";
+            default:
+                return null;
+        }
+    }
 }
