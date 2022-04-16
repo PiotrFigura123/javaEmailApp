58ba3a45c1b0ad7c5688c24d9a4841da7c70bd26
Piotr Figura <pf>
2022-04-16 13:41:22 +0200

    Fetch folder service

--- src/com/barosanu/EmailMenager.java
+++ src/com/barosanu/EmailMenager.java
@@ -1,19 +1,22 @@
 package com.barosanu;
 
+import com.barosanu.controller.services.FetchFolderService;
 import com.barosanu.model.EmailAccount;
+import com.barosanu.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
 public class EmailMenager {
 
 
     //Folder handling:
-    private TreeItem<String> foldersRoot = new TreeItem<String>("");
-    public TreeItem<String> getFoldersRoot() {
+    private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
+    public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
     public void addEmailAccount(EmailAccount emailAccount){
-        TreeItem<String> treeItem = new TreeItem<String>(emailAccount.getAddress());
-        treeItem.setExpanded(true);
+        EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
+        FetchFolderService fetchFolderService = new FetchFolderService(emailAccount.getStore(),treeItem);
+        fetchFolderService.start();
         foldersRoot.getChildren().add(treeItem);
 
     }
---
+++ src/com/barosanu/controller/services/FetchFolderService.java
@@ -0,0 +1,43 @@
+package com.barosanu.controller.services;
+
+import com.barosanu.model.EmailTreeItem;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+import javax.mail.Folder;
+import javax.mail.MessagingException;
+import javax.mail.Store;
+
+public class FetchFolderService extends Service<Void> {
+
+    private Store store;
+    private EmailTreeItem<String> foldersRoot;
+
+    public FetchFolderService(Store store, EmailTreeItem<String> foldersRoot) {
+        this.store = store;
+        this.foldersRoot = foldersRoot;
+    }
+
+    @Override
+    protected Task<Void> createTask() {
+        return new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                fetchFolders();
+                return null;
+            }
+        };
+    }
+
+    private void fetchFolders() throws MessagingException {
+        Folder[] folders = store.getDefaultFolder().list();
+        handleFolders(folders, foldersRoot);
+    }
+
+    private void handleFolders(Folder[] folders, EmailTreeItem<String> foldersRooting) {
+        for(Folder folder:folders){
+            EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
+            foldersRoot.getChildren().add(emailTreeItem);
+        }
+    }
+}
