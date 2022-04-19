0c76de352cd3b5b9113c2485107c2e497f16f40a
Piotr Figura <pf>
2022-04-19 19:46:19 +0200

    different size display

--- src/com/barosanu/model/EmailMessage.java
+++ src/com/barosanu/model/EmailMessage.java
@@ -11,7 +11,7 @@ public class EmailMessage {
     private SimpleStringProperty subject;
     private SimpleStringProperty sender;
     private SimpleStringProperty recipent;
-    private SimpleIntegerProperty size;
+    private SimpleObjectProperty<SizeInteger> size;
     private SimpleObjectProperty<Date> date;
     private boolean isRead;
     private Message message;
@@ -19,7 +19,7 @@ public class EmailMessage {
         this.subject = new SimpleStringProperty(subject);
         this.sender = new SimpleStringProperty(sender);
         this.recipent = new SimpleStringProperty(recipient);
-        this.size = new SimpleIntegerProperty(size);
+        this.size = new SimpleObjectProperty<SizeInteger>(new SizeInteger(size));
         this.date = new SimpleObjectProperty<Date>(date);
         this.isRead = isRead;
         this.message = message;
@@ -35,7 +35,7 @@ public class EmailMessage {
     public String getRecipent(){
         return this.recipent.getName();
     }
-    public Integer getSize(){
+    public SizeInteger getSize(){
         return this.size.get();
     }
     public Date getDate(){
---
+++ src/com/barosanu/model/SizeInteger.java
@@ -0,0 +1,21 @@
+package com.barosanu.model;
+
+public class SizeInteger {
+    private int size;
+
+    public SizeInteger(int size) {
+        this.size = size;
+    }
+    @Override
+    public String toString(){
+        if(size<=0){
+            return "0";
+        }else if(size<1024){
+            return size + " B";
+        }else if (size<1038576){
+            return size/1024 + "kB";
+        }else{
+            return size/1048576 + " MB";
+        }
+    }
+}
