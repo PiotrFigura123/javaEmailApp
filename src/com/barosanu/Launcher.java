58dd402c20e2cf2454c65e27263d7f621ed4c5fc
Piotr Figura <pf>
2022-04-25 20:22:19 +0200

    save data do file.ser

--- src/com/barosanu/Launcher.java
+++ src/com/barosanu/Launcher.java
@@ -1,5 +1,8 @@
 package com.barosanu;
 
+import com.barosanu.controller.persistance.Persistance;
+import com.barosanu.controller.persistance.ValidAccount;
+import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,19 +12,30 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Launcher extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
+    private Persistance persistance = new Persistance();
+    private EmailMenager emailMenager = new EmailMenager();
+
     @Override
     public void start(Stage stage) throws Exception {
-        ViewFactory viewFactory = new ViewFactory(new EmailMenager());
+        ViewFactory viewFactory = new ViewFactory(emailMenager);
         viewFactory.showLoginWindow();
-
-
-
     }
 
+    @Override
+    public void stop() throws Exception {
+        List<ValidAccount> validAccountList = new ArrayList<ValidAccount>();
+        for (EmailAccount emailAccount:emailMenager.getEmailAccounts()){
+            validAccountList.add(new ValidAccount(emailAccount.getAddress(), emailAccount.getPassword()));
+        }
+        persistance.saveToPersistance(validAccountList);
+    }
 }
